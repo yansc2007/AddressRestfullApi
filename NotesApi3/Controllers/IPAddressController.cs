5c0fd3503798709066be8cf3026b5e24d1c7ab39
颜士诚 <yansc@21cn.com>
2017-07-17 10:46:05 +0800

    C# VS2013 ASP.NET MVC Restfull Api

---
+++ NotesApi3/Controllers/IPAddressController.cs
@@ -0,0 +1,29 @@
+﻿using NotesApi3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace NotesApi3.Controllers
+{
+    public class IPAddressController : ApiController
+    {
+        private static IList<Address> addresses = new List<Address>  
+        {   
+            new Address(){ IPAddress="1.91.38.31", Province="北京市", City="北京市" },     
+            new Address(){ IPAddress = "210.75.225.254", Province = "上海市", City = "上海市"  },  
+        };
+
+        public IEnumerable<Address> GetIPAddresses()
+        {
+            return addresses;
+        }
+
+        public Address GetIPAddressByIP(string IP)
+        {
+            return addresses.FirstOrDefault(x => x.IPAddress == IP);
+        }  
+	}
+}
